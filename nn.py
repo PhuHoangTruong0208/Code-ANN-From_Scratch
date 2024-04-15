ff407383525fe0b6d699674d0525ccc3bf18393d
Phu Hoang <152080248+PhuHoangTruong0208@users.noreply.github.com>
2024-04-16 00:06:33 +0700

    Update nn.py

--- nn.py
+++ nn.py
@@ -49,12 +49,12 @@ class NeuralNetwork:
     def compute_cost(self, y, y_pred):
             return np.mean(np.square(y - y_pred))
     
-    def backward(self, layers, x, y, epochs=2, lr=0.001, limit_finding=1000):
+    def backward(self, layers, x, y, epochs=2, lr=0.001, limit_cost_decimal=3, limit_grad_finding=100000):
         assert isinstance(layers, list), "cần 1 danh sách chứa các layers"
         pre_loss = 1e100
         break_all = False
         for i in range(epochs):
-            for f in range(limit_finding):
+            for f in range(limit_grad_finding):
                 layers_op_final = x
                 for layer in layers:
                     layer_output = layer.forward(layers_op_final)
@@ -65,10 +65,10 @@ class NeuralNetwork:
 
                 loss = self.compute_cost(y_pred=layers_op_final, y=y)
 
-                if f+10 >= limit_finding:
+                if f+10 >= limit_grad_finding:
                     break_all = True
 
-                if loss < pre_loss:
+                if round(loss, limit_cost_decimal) < round(pre_loss, limit_cost_decimal):
                     pre_loss = loss
                     break
 
@@ -91,23 +91,49 @@ x = np.array([
               [-4.663450296841878, 12.979621321604862, -9.72758766810308, 9.17412767057377], 
               [-7.117883096789941, 9.805993492085843, 3.595913414267891, -0.3916747814772092], 
               [0.262632533772451, -4.6621570838899915, -2.6180871559568883, 3.575197991828804], 
-              [16.028124156440498, -6.315977482458913, 2.4777774443417075, -4.1628653375100715]
+              [16.028124156440498, -6.315977482458913, 2.4777774443417075, -4.1628653375100715],
+              [-1.645301691263, 3.0841715827615026, 12.03387711923264, -3.3586880359025404], 
+              [-4.663450296841878, 12.979624636634862, -9.72745546810308, 9.17412767057377], 
+              [-7.117883096789941, 9.805993492085843, 3.5954232267891, -0.391875814772092], 
+              [0.262632533772451, -4.6621570838899915, -2.61808355368883, 3.575197991828804], 
+              [16.028124156440498, -6.31524358913, 2.4777774443417075, -4.1628653375100715],
+              [-1.645301691263, 3.0841715827615026, 12.03387711923264, -3.3586880359025404], 
+              [-4.663450296841878, 12.979624636634862, -9.72745546810308, 9.17412767057377], 
+              [-7.117883096789941, 9.805993492085843, 3.5954232267891, -0.391875814772092], 
+              [0.262632533772451, -4.6621570838899915, -2.61808355368883, 3.575197991828804], 
+              [16.028124156440498, -6.31524358913, 2.4777774443417075, -4.1628653375100715]
 ])
-y = []
-for i in range(5):
-    y.append([i])
-y = np.array(y)
+
+y = np.array([
+    [0],
+    [1],
+    [2],
+    [3],
+    [4],
+    [0],
+    [1],
+    [2],
+    [3],
+    [4], 
+    [0],
+    [1],
+    [2],
+    [3],
+    [4]])
 
 # mô hình phân loại đa lớp
 layers = [
-NeuralNetwork(input_dim=4, hidden_dim=64, activation="relu"),
-NeuralNetwork(input_dim=64, hidden_dim=1000, activation="relu"),
-NeuralNetwork(input_dim=1000, hidden_dim=500, activation="relu"),
-NeuralNetwork(input_dim=500, hidden_dim=len(y), activation="softmax")
+NeuralNetwork(input_dim=4, hidden_dim=1000, activation="tanh"),
+NeuralNetwork(input_dim=1000, hidden_dim=1000, activation="tanh"),
+NeuralNetwork(input_dim=1000, hidden_dim=1000, activation="tanh"),
+NeuralNetwork(input_dim=1000, hidden_dim=1000, activation="tanh"),
+NeuralNetwork(input_dim=1000, hidden_dim=1000, activation="tanh"),
+NeuralNetwork(input_dim=1000, hidden_dim=4, activation="softmax")
 ]
 
 model = NeuralNetwork()
-model.backward(layers=layers, x=x, y=y, epochs=300, lr=0.001)
+model.backward(layers=layers, x=x, y=y, epochs=500, lr=0.001, limit_cost_decimal=4,
+                limit_grad_finding=200)
 
 # thử nghiệm mô hình
 while True:
